ce053591c3685d383976ff1f6d1ff5a6bae6364c
Jakob Luter <jakobluter20@gmail.com>
2022-11-22 13:13:34 -0600

    Add initial project setup w/ typescript

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+import Header from './Components/Header/Header';
+
+
+const App: React.FC = () => {
+  return (<div>
+    <Header/>
+  </div>
+  )
+}
+
+export default App;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import './Header.scss';
+import Headshot from '../../assets/profile-pic.jpg';
+
+
+const Header: React.FC = (props) => {
+
+
+    return <header>
+        <figure>
+            <img src={Headshot} alt="site-logo"/>
+        </figure>
+        <nav>
+            <ul>
+                <li>About</li>
+                <li>Projects</li>
+            </ul>
+        </nav>
+    </header>;
+}
+
+export default Header
