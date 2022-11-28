07dc0c4b7625c758ecaee850fb09602b7699168b
Jakob Luter <jakobluter20@gmail.com>
2022-11-28 12:48:41 -0600

    Add first draft HTML/CSS for line animation.

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.scss';
 import './styles/_global.scss';
 import Header from './Components/Header/Header';
+import LineAnimation from './Components/LineAnimation/LineAnimation';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const App: React.FC = () => {
   return (<div>
     <Header/>
+    <LineAnimation />
   </div>
   )
 }
--- src/components/LineAnimation/LineAnimation.tsx
+++ src/components/LineAnimation/LineAnimation.tsx
@@ -3,8 +3,38 @@ import './LineAnimation.scss';
 
 const LineAnimation: React.FC = () => {
     return (
-        <div></div>
-    )
+      <div id="footer-navmenu">
+        <svg
+          className="wave-animation"
+          preserveAspectRatio="none"
+          viewBox="0 24 150 28"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+        >
+          <defs>
+            <path
+              d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"
+              id="gentle-wave"
+            />
+          </defs>
+          <g className="wave-bg">
+            <use
+              fill="rgba(242,193,78,.5)"
+              x="50"
+              xlinkHref="#gentle-wave"
+              y="0"
+            />
+            <use
+              fill="rgba(242,193,78,.7)"
+              x="50"
+              xlinkHref="#gentle-wave"
+              y="3"
+            />
+            <use fill="#344E41" x="50" xlinkHref="#gentle-wave" y="6" />
+          </g>
+        </svg>
+      </div>
+    );
 }
 
 export default LineAnimation;
