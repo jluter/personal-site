2dcab6945a1e86323fbe11e82fd74f9b40dad437
Jakob Luter <jakobluter20@gmail.com>
2022-11-29 13:58:55 -0600

    Add animations and adjustments for tablet breakpoint.

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -9,7 +9,7 @@ const AboutMe: React.FC = () => {
         </div>
         <div>
             <p className='about-me-description'>
-            I'm a web developer/designer that has recently undergone a career change from social services. Working for people with disablities has made me passionate about building accessible, intuitive applications. 
+            I'm a web developer that has recently undergone a career change from the social services field. Working for people with disablities has made me passionate about building accessible, intuitive applications. 
             </p>
         </div>
       </div>
