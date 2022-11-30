50b8eb44cbc3cc0e60a8b5bfc843e0219298d3d4
Jakob Luter <jakobluter20@gmail.com>
2022-11-29 23:32:30 -0600

    Add description component for desktop breakpoint. Adjust scss for sizing.

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,17 @@ import Header from './Components/Header/Header';
 import LineAnimation from './Components/LineAnimation/LineAnimation';
 import AboutMe from './Components/AboutMe/AboutMe';
 import Footer from './Components/Footer/Footer';
+import DesktopDescription from './Components/DesktopDescription/DesktopDescription';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const App: React.FC = () => {
   return (<div>
     <Header/>
     <LineAnimation />
+    <main className='main-container'>
     <AboutMe />
+    <DesktopDescription />
+    </main>
     <Footer />
   </div>
   )
