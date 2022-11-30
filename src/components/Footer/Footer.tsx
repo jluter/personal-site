2d4ffd9c5f25a1fc8c487e59e86c74800682f9c6
Jakob Luter <jakobluter20@gmail.com>
2022-11-29 18:30:45 -0600

    Add fade in animations and mixins to Footer.scss

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,18 +6,21 @@ import { HiMail } from 'react-icons/hi';
 
 
 const Footer: React.FC = () => {
+
+    const windowWidth: number = window.innerWidth;
+
     return (
         <footer className='footer'>
             <nav className='footer__nav'>
                 <div className='footer__nav-item'>
-                    <FaPhoneAlt size='1rem' /><p className='footer__nav-item--info'>: (936) 346-9878</p>
+                    <FaPhoneAlt size={(windowWidth > 767) ? '1.5rem' : '1rem'} /><p className='footer__nav-item--info'>: (936) 346-9878</p>
                 </div>
                 <div className='footer__line-animation'>
                 <LineAnimation />
 
                 </div>
                 <div className='footer__nav-item'>
-                    <HiMail size='1.5rem'/><p className='footer__nav-item--info'>: jakobluter20@gmail.com</p>
+                    <HiMail size={(windowWidth > 767) ? '2rem' : '1.5rem'}/><p className='footer__nav-item--info'>: jakobluter20@gmail.com</p>
                 </div>
             </nav>
         </footer>
