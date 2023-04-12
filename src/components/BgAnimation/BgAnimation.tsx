99130d51cae66a299f30c8f03e8206277662b61e
Jakob Luter <jakobluter20@gmail.com>
2023-04-12 00:28:16 -0500

    Add BgAnimation component

---
+++ src/components/BgAnimation/BgAnimation.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+const BgAnimation: React.FC = () => {
+
+//P5.JS CODE BELOW--------------
+// function setup(){
+//     createCanvas(720,400);
+//   }
+  
+//   // the parameter at which x and y depends is usually taken as either t or symbol of theta
+//   let t = 0;
+//   function draw(){
+//     background(0);
+//     translate(width/2,height/2);
+//     stroke((255));
+//     strokeWeight(1);
+//     //loop for adding 100 lines
+//     for(let i = 0;i<100;i++){
+//       line(x1(t+i),y1(t+i),x2(t+i)*0.5,y2(t+i)*0.5);
+//     }
+//     t+=0.10;
+//   }
+//   // function to change initial x co-ordinate of the line
+//   function x1(t){
+//     return sin(t/10)*125+sin(t/20)*125+sin(t/30)*125;
+//   }
+  
+//   // function to change initial y co-ordinate of the line
+//   function y1(t){
+//     return cos(t/10)*125+cos(t/20)*125+cos(t/30)*125;
+//   }
+  
+//   // function to change final x co-ordinate of the line
+//   function x2(t){
+//     return sin(t/15)*125+sin(t/25)*125+sin(t/35)*125;
+//   }
+  
+//   // function to change final y co-ordinate of the line
+//   function y2(t){
+//     return cos(t/15)*25+cos(t/25)*25+cos(t/35)*25;
+//   }
+  
+  
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default BgAnimation;
