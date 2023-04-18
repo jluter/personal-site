9543ae4b99cb64ecda4caa5ddbeab66912786a60
Jakob Luter <jakobluter20@gmail.com>
2023-04-17 19:50:27 -0500

    Change threejsscene.tsx to be a cleaner example. Cleaning function at end of useEffect. resize camera and canvas on window resize.

--- src/components/ThreejsScene/ThreejsScene.tsx
+++ src/components/ThreejsScene/ThreejsScene.tsx
@@ -1,99 +1,66 @@
 import React, { useRef, useEffect } from "react";
 import * as THREE from "three";
-import grain from '../../assets/images/grain.jpg';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 const ThreejsScene: React.FC = () => {
+  
+  const canvasRef = useRef<HTMLDivElement>(null); //Tells React where to mount three.js scene/canvas
+  
+  useEffect(() => {
+    const scene = new THREE.Scene();
+    
+    const canvas = canvasRef.current;
+    const camera = new THREE.PerspectiveCamera(
+      50, 
+      window.innerWidth / 
+      window.innerHeight,
+      0.001,
+      1000
+      );
+      camera.position.z = 5;
+      
+      const renderer = new THREE.WebGLRenderer();
+      renderer.setSize(window.innerWidth, window.innerHeight);
+    if (canvas) {
+      canvas.appendChild(renderer.domElement);
 
-// const scene: any = () => {
-//   const scene = new THREE.Scene();
-//   const camera = new THREE.PerspectiveCamera(
-//     75,
-//     window.innerWidth / window.innerHeight,
-//     0.1,
-//     1000
-//   );
+    }
 
-//   const renderer = new THREE.WebGLRenderer();
-//   renderer.setSize(window.innerWidth, window.innerHeight);
-//   document.body.appendChild(renderer.domElement);
+    const geometry = new THREE.BoxGeometry( 1, 2, 1.5);
+    const material = new THREE.MeshBasicMaterial ( {color: 0x00ff00} );
+    const cube = new THREE.Mesh (geometry, material);
+    scene.add(cube);
 
-//   const geometry = new THREE.BoxGeometry(1, 1, 1);
-//   const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-//   const cube = new THREE.Mesh(geometry, material);
-//   scene.add(cube);
+    const animate = () => {
+      requestAnimationFrame( animate );
 
-//   camera.position.z = 5;
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
 
-//   function animate() {
-//     requestAnimationFrame(animate);
+      renderer.render( scene, camera );
+    }
 
-//     cube.rotation.x += 0.01;
-//     cube.rotation.y += 0.01;
+    let onWindowResize = function () {
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix();
+      renderer.setSize( window.innerWidth, window.innerHeight );
+    }
 
-//     renderer.render(scene, camera);
-//   }
+    window.addEventListener("resize", onWindowResize, false);
 
-//   animate();
-// };
+    animate();
 
-const newScene: any = () => {
-  // Scene
-const scene = new THREE.Scene();
+    const test2 = document.getElementById("test2")
 
-// Camera
-const fov = 75;
-const aspect = window.innerWidth / window.innerHeight;
-const near = 1;
-const far = 1000;
-const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera.position.z = 10;
+    return () => {
+      if (canvas) {
 
-// Disc
-const radius = 5;
-const depth = 1;
-const segments = 32;
-const shape = new THREE.Shape();
-shape.moveTo(radius, 0);
-for (let i = 1; i <= segments; i++) {
-  const theta = (i / segments) * Math.PI * 2;
-  const x = radius * Math.cos(theta);
-  const y = radius * Math.sin(theta);
-  shape.lineTo(x, y);
-}
-const extrudeSettings = { depth: depth, bevelEnabled: false };
-const geometry = new THREE.ExtrudeGeometry(shape, extrudeSettings);
-const texture = new THREE.TextureLoader().load(grain);
+        canvas.removeChild( renderer.domElement );
+    }};
+  }, []);
 
-texture.wrapS = THREE.RepeatWrapping;
-texture.wrapT = THREE.RepeatWrapping;
-texture.repeat.set(radius * 2, radius * 2);
-
-const material = new THREE.MeshBasicMaterial({ map: texture });
-const mesh = new THREE.Mesh(geometry, material);
-mesh.rotation.y = -Math.PI / 4;
-scene.add(mesh);
-
-// Renderer
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-// Animation loop
-const animate = function () {
-  requestAnimationFrame(animate);
-  const angle = Date.now() * 0.001;
-  mesh.rotation.z = angle/5;
-  renderer.render(scene, camera);
-};
-
-animate();
-}
   return (
-    <div>
-      {/* <script type="module" src={scene()}></script> */}
-      BREAK
-      <script type='module' src={newScene()}></script>
-    </div>
+      <div id="test2" ref={canvasRef}></div>
   );
 };
 
