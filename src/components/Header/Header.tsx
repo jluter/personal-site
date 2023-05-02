1ec6fb817602c46551d7b976f07f823f38e61496
Jakob Luter <jakobluter20@gmail.com>
2023-05-01 22:32:39 -0500

    Add return to original THREEjs camera position on name lcick,

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -22,7 +22,7 @@ const Header: React.FC<Props> = ({cameraState, handleHeaderListClick}) => {
 
   return (
     <header className="header">
-      <h1 className="header__title">Jakob{pagebreak}Luter</h1>
+      <h1 className="header__title" onClick={() => handleHeaderListClick(0, 0, 5)}>Jakob{pagebreak}Luter</h1>
       <ul className='header__list'>
         <li className='header__list-item' onClick={() => handleHeaderListClick(1, 1, 3)}>Works</li>
         <li className='header__list-item' onClick={() => handleHeaderListClick(-1, 1, 3)}>About</li>
