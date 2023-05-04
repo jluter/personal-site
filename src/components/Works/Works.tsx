ae08bd5dbf7a594157f172cf2f1f74c3c5f04835
Jakob Luter <jakobluter20@gmail.com>
2023-05-04 15:52:28 -0500

    Add mask-image fade in/out on works list.

--- src/components/Works/Works.tsx
+++ src/components/Works/Works.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Works.scss';
 // import { SiGithub, SiLinkedin } from 'react-icons/si';
 
@@ -7,18 +7,11 @@ import './Works.scss';
 
 const Works: React.FC = () => {
 
-    const [workPosition, setWorkPosition] = useState<number>(0);
-
-    const handleWorkListScroll = (event: React.UIEvent<HTMLDivElement>) => {
-        let scrollTop = event.currentTarget.scrollTop;
-        console.log(scrollTop);
-        setWorkPosition(scrollTop + 100);
-    }
 
     return(
         <div className='works-container'>
             <div className='works-list'>
-                <div className='works-list__item' onScroll={(event) => handleWorkListScroll(event)} style={{color: `rgba(0,0,0,${workPosition})`}}>Work1</div>
+                <div className='works-list__item'>Work1</div>
                 <div className='works-list__item'>Work2</div>
                 <div className='works-list__item'>Work3</div>
                 <div className='works-list__item'>Work4</div>
