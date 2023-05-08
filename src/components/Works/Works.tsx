52430602b166b58ea63259fb3f0324930df82504
Jakob Luter <jakobluter20@gmail.com>
2023-05-08 17:01:01 -0500

    Add mapping and short circuiting for when a work is clicked.

--- src/components/Works/Works.tsx
+++ src/components/Works/Works.tsx
@@ -1,38 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Works.scss';
+import ranchWebsiteImage from '../../assets/images/ranch-website.jpg';
 // import { SiGithub, SiLinkedin } from 'react-icons/si';
 
 
 
 
 const Works: React.FC = () => {
+    
+    //List of works to be added.
+  // ranch-website
+  // pollutionpoll
+  // 12right.com
+  // shercousa
+  // factoryonesherco
+  // factoryonemotorsports
+  // vault.shercousa
+  // sanantoniochessclub
+  // completechess
+  // completechessfoundation
+  
+  const [worksListState, setWorksListState] = useState("");
+  const worksArray = [
+    {
+      title: "Ranch Website",
+      img: ranchWebsiteImage,
+      description:
+        "A mockup site for a local farm/ranch. Features custom styling and WordPress theme building.",
+    },
+    {title: 'Pollution Poll'},
+    {title: '12Right'},
+    {title: 'Sherco USA'},
+    {title: 'FactoryONE Sherco'},
+    {title: 'FactoryONE Motorsports'},
+    {title: 'Sherco Vault'},
+    {title: 'San Antonio Chess Club'},
+    {title: 'Complete Chess'},
+    {title: 'Complete Chess Foundation'},
+  ];
 
-    //List of works to be added. 
-    // pollutionpoll
-    // ranch-website
-    // shercousa
-    // factoryonesherco
-    // factoryonemotorsports
-    // vault.shercousa
-    // sanantoniochessclub
-    // completechess
-    // completechessfoundation
+  const handleWorksTitleClick = (title: string) => {
+    setWorksListState(title);
+  }
 
-
-
-    return(
-        <div className='works-container'>
-            <div className='works-list'>
-                <div className='works-list__item'>Work1</div>
-                <div className='works-list__item'>Work2</div>
-                <div className='works-list__item'>Work3</div>
-                <div className='works-list__item'>Work4</div>
-                <div className='works-list__item'>Work5</div>
-                <div className='works-list__item'>Work6</div>
-                <div className='works-list__item'>Work7</div>
-            </div>
+  return (
+    <div className="works-container">
+      <ul className="works-list">
+        {worksArray.map((work) => (
+            <li onClick={() => {handleWorksTitleClick(work.title)}} className='works-list__item'>{work.title}</li>
+        ))}
+      </ul>
+      {worksListState && 
+      <article>
+        <div>
+            <p>{worksListState}</p>
         </div>
-    );
+      </article>
+      }
+    </div>
+  );
 };
 
 export default Works;
