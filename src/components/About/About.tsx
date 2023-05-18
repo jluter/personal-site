fd68120d482d445abaa33d44f82be1b78f5959b1
Jakob Luter <jakobluter20@gmail.com>
2023-05-17 21:05:55 -0500

    Add minor changes and styling for desktop breakpoint.

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -12,7 +12,7 @@ const About: React.FC<Props> = ({handleHeaderListClick}) => {
   return (
     <div className="about">
       <p className="about__paragraph">Thank you for visiting. I'm a full stack developer interested in learning,
-      collaborating, and exploring new opportunities.</p><p className="about__paragraph">Outside of tech, I feel passionate about the environment and tabletop gaming. Feel free to <span className="about__contact-me" onClick={() => {
+      collaborating, and exploring new opportunities.</p><p className="about__paragraph">Outside of tech, I feel passionate about the environment, music, and tabletop gaming. Feel free to <span className="about__contact-me" onClick={() => {
         handleHeaderListClick(x, -1, 2.5, 'contact')
       }}>contact me</span> for any personal or professional inquiries. </p>
       <p className="about__paragraph">Please take a look at my <a className="about__link" href={resume} target="_blank" rel="noreferrer noopener">resume</a>, or my <a className="about__link" href="https://stackshare.io/jluter/my-stack" target="_blank" rel="noreferrer noopener">tech stack</a>.</p> 
